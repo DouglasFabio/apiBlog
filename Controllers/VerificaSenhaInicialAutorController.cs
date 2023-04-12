dc4f21a769cebe62d268bcff9f9138e4df6a4d8b
Douglas <douglasfabio.ifsp@gmail.com>
2023-04-12 16:37:09 -0300

    Verificação Contas - Autor e Leitor

---
+++ Controllers/VerificaSenhaInicialAutorController.cs
@@ -0,0 +1,56 @@
+using apiBlog.Models;
+namespace apiBlog.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VerificaSenhaInicialAutorController : ControllerBase
+{
+    private readonly SeBlogContext context;
+
+    public VerificaSenhaInicialAutorController(SeBlogContext Context)
+    {
+        context = Context;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put([FromBody] TbUsuario model)
+    {
+        if (await context.TbUsuarios.AnyAsync(p => p.CodAtivacao == model.CodAtivacao) == false)
+            return BadRequest("Código inválido, tente novamente.");
+        
+        try
+        {
+            if (await context.TbUsuarios.AnyAsync(p => p.CodAtivacao == model.CodAtivacao)){
+                TbUsuario usuario = new TbUsuario();
+                usuario.Nome = usuario.Nome;
+                usuario.Email = usuario.Email;
+                usuario.Senha = usuario.Senha;
+                usuario.StatusConta = "V";
+
+                context.TbUsuarios
+                        .Where(u => u.CodAtivacao == model.CodAtivacao)
+                        .ExecuteUpdate(s =>
+                            s.SetProperty(u => u.StatusConta, usuario.StatusConta)
+                        );
+                
+                await context.SaveChangesAsync();
+                
+                usuario.CodAtivacao = null;
+                context.TbUsuarios
+                        .Where(u => u.CodAtivacao == model.CodAtivacao)
+                        .ExecuteUpdate(s =>
+                            s.SetProperty(u => u.CodAtivacao, usuario.CodAtivacao)
+                        );
+
+                return Ok("Conta verificada com sucesso!");
+            }else 
+                return BadRequest("Não foi possível verificar conta.");        
+        }
+        catch
+        {
+            return BadRequest();
+        }
+    }
+}
